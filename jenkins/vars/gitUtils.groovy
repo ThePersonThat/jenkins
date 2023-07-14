2518287746191e6058c3f9eea5aa8d4a4c323e73
ThePersonThat <gaudabrus@inbox.ru>
2023-07-14 14:32:53 +0300

    Jenkinsfile

--- jenkins/vars/gitUtils.groovy
+++ jenkins/vars/gitUtils.groovy
@@ -1,10 +1,6 @@
 def getCurrentBranch() {
-    return env.BRANCH_NAME
-}
-
-def getCurrentBranchCli() {
     return sh(
             script: 'git rev-parse --abbrev-ref HEAD',
             returnStdout: true
-    )
+    ).trim()
 }
