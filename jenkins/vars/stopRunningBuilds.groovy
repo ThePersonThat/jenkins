21e3e32fb3fc960498e0a7d4212e6b026a38891d
ThePersonThat <gaudabrus@inbox.ru>
2023-07-14 17:52:25 +0300

    Running builds

--- jenkins/vars/stopRunningBuilds.groovy
+++ jenkins/vars/stopRunningBuilds.groovy
@@ -1,6 +1,29 @@
-def call() {
-    def jobName = env.JOB_NAME
-    def buildNumber = env.BUILD_NUMBER.toInteger()
-    def currentJob = Jenkins.instance.getItemByFullName(jobName)
+import jenkins.model.Jenkins
 
+def call() {
+    def currentJob = getCurrentJob()
+
+    currentJob.builds
+        .findAll(this.&filterBuilds)
+        .each { it.doStop() }
+}
+
+def filterBuilds(def build) {
+    return build.isBuilding() && isPreviousBuild(build) && isSameBranch(build)
+}
+
+def isPreviousBuild(def build) {
+    return build.number.toInteger() < env.BUILD_NUMBER.toInteger()
+}
+
+def isSameBranch(def build) {
+    return env.BRANCH_NAME.equals(getBuildBranch(build))
+}
+
+def getBuildBranch(def build) {
+    return build.getBuildVariables.BRANCH_NAME
+}
+
+def getCurrentJob() {
+    return Jenkins.instance.getItemByFullName(env.JOB_NAME)
 }
