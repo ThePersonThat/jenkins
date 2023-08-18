282acb5895473e79ca67838ecdfd2dcdf66a73f5
ThePersonThat <gaudabrus@gmail.com>
2023-08-18 23:25:58 +0300

    test

--- jenkins/vars/runCheckstyle.groovy
+++ jenkins/vars/runCheckstyle.groovy
@@ -6,7 +6,7 @@ def call() {
 
         recordIssues(
                 aggregatingResults: true,
-                publishAllIssues: false,
+                publishAllIssues: isMasterBranch(),
                 qualityGates: [getQualityGate()],
                 tools: [
                         java(),
@@ -17,7 +17,7 @@ def call() {
 }
 
 def getQualityGate() {
-    def type = isMasterBranch() ? 'TOTAL' : 'NEW_ERROR'
+    def type = isMasterBranch() ? 'TOTAL' : 'TOTAL'
 
     return [threshold: 1, type: "${type}"]
 }
